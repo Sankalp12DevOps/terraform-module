c3c15a8a7494491a95f03811702b4b53c0576cbf
Sankalp12DevOps <sravanthi388@gmail.com>
2023-04-24 20:11:10 +0530

    m

--- main.tf
+++ main.tf
@@ -38,8 +38,97 @@ resource "aws_internet_gateway" "gw" {
   }
 }
 
-resource "aws_vpc_peering_connection" "foo" {
-  peer_vpc_id   = var.PEER_VPC_ID
-  vpc_id        = aws_vpc.main.id
+
+resource "aws_vpc_peering_connection" "roboshop_peering" {
+  peer_vpc_id   = aws_vpc.main.id
+  vpc_id        = var.PEER_VPC_ID
   auto_accept   = true
 }
+
+//public route table
+
+resource "aws_route_table" "publicRoute" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block                = var.DEFAULT_CIDR
+    vpc_peering_connection_id = aws_vpc_peering_connection.roboshop_peering.id
+  }
+   route {
+    cidr_block = var.ALLOW_ALL_CIDR
+    gateway_id = aws_internet_gateway.gw
+  }
+  tags = {
+    Name = "publicRouteTable-${var.ENV}"
+  }
+}
+
+//public subnet route table association
+resource "aws_route_table_association" "publicRoute" {
+  count          = length(aws_subnet.public.*.id)
+  subnet_id      = element(aws_subnet.public.*.id,count.index)
+  route_table_id = aws_route_table.publicRoute.id
+}
+
+
+// Private route table
+
+resource "aws_route_table" "privateRoute" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block                = var.DEFAULT_CIDR
+    vpc_peering_connection_id = aws_vpc_peering_connection.roboshop_peering.id
+  }
+   route {
+    cidr_block     = var.ALLOW_ALL_CIDR
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+  tags = {
+    Name = "privateRouteTable-${var.ENV}"
+  }
+}
+
+//private subnet route table association
+resource "aws_route_table_association" "publicRoute" {
+  count          = length(aws_subnet.private.*.id)
+  subnet_id      = element(aws_subnet.private.*.id,count.index)
+  route_table_id = aws_route_table.privateRoute.id
+}
+
+
+
+
+
+// EIP Creation
+resource "aws_eip" "eip" {
+  vpc      = true
+  tags {
+    Name = "eip-roboshop-${var.ENV}"
+  }
+}
+
+//NAt gateway
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.eip.id
+  subnet_id     = aws_subnet.public.*.id[1]
+
+  tags = {
+    Name = "gw NAT-${var.ENV}"
+  }
+
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+  depends_on = [aws_eip.eip]
+}
+
+
+#Default route table association
+
+resource "aws_route" "r" {
+  route_table_id            = var.DEFAULT_RT
+  destination_cidr_block    = var.VPC_CIDR
+  vpc_peering_connection_id = aws_vpc_peering_connection.roboshop_peering.id
+  depends_on                = [aws_vpc_peering_connection.roboshop_peering]
+}
+
