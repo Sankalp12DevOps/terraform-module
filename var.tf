1d90d1038d42d4b28a107a30b614e0eea25071d6
Sankalp12DevOps <sravanthi388@gmail.com>
2023-05-04 21:00:45 +0530

    m

--- var.tf
+++ var.tf
@@ -8,6 +8,6 @@ variable DEFAULT_CIDR {}
 variable ALLOW_ALL_CIDR {}
 variable DEFAULT_RT{}
 variable PRIVATE_HOSTED_ZONE_ID{}
-# variable PRIVATE_HOSTED_ZONE_NAME {}
-# variable PUBLIC_HOSTED_ZONE_ID {}
-# variable PUBLIC_HOSTED_ZONE_NAME {}
+variable PRIVATE_HOSTED_ZONE_NAME {}
+variable PUBLIC_HOSTED_ZONE_ID {}
+variable PUBLIC_HOSTED_ZONE_NAME {}
